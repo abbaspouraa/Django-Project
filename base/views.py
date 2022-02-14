ee5dff04bbbac1c9df40796c959ab74e6568a3b0
Amir Abbaspour <83620288+abbaspouraa@users.noreply.github.com>
2022-02-14 14:04:37 -0700

    http request to templates

--- base/views.py
+++ base/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+
+# def home(request):
+#     return HttpResponse('Home Page')
+
+# def room(request):
+#     return HttpResponse('Room')
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def room(request):
+    return render(request, 'room.html')
